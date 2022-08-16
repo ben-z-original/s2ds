ae282f5e7984fc2fdfed13397eaa06136113c243
Christian Benz <christian.benz@uni-weimar.de>
2022-08-16 18:03:07 +0200

    Add evaluate.py

---
+++ utils/evaluate.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import numpy as np
+from torch import from_numpy
+from torchmetrics import JaccardIndex
+from utils import ltIoUMetric, pred2class, lab2class, boundary_tolerance
+
+import matplotlib
+
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    files = np.arange(650, 743)
+    # files.sort()
+
+    pred_paths = ["results/ensemble/STRUDEF_{}.png",
+                  "../hma/7_argmax/{}.png"]
+    true_path = "../test/{}_lab.png"
+
+    pred_acc = [np.empty((0, 1024, 1024), np.uint8)] * len(pred_paths)
+    true_acc = np.empty((0, 1024, 1024), np.uint8)
+
+    for f in files[:5]:
+        print(f)
+
+        # get predictions
+        for i, p in enumerate(pred_paths):
+            pred = cv2.imread(p.format(f), cv2.IMREAD_COLOR)
+            pred = cv2.cvtColor(pred, cv2.COLOR_BGR2RGB)
+            pred = pred2class(pred)
+            pred_acc[i] = np.append(pred_acc[i], pred.reshape(1, 1024, 1024), axis=0)
+
+        # get label
+        true = cv2.imread(true_path.format(f), cv2.IMREAD_COLOR)
+        true = cv2.cvtColor(true, cv2.COLOR_BGR2RGB)
+        true = lab2class(true)
+        true = boundary_tolerance(true)
+        true_acc = np.append(true_acc, true.reshape(1, 1024, 1024), axis=0)
+
+    tolerances = [0, 2, 4, 8, 16, 32]
+
+    iou = np.empty((len(pred_acc), 7))
+    ltiou = np.empty((len(pred_acc), len(tolerances), 2))
+
+    for i in range(len(pred_acc)):
+        # non-crack
+        iou[i, ...] = JaccardIndex(num_classes=8, average=None, ignore_index=7)(from_numpy(pred_acc[i]),
+                                                                                from_numpy(true_acc))
+        print(iou[i, ...])
+        # crack
+        pred_acc[i] = np.where((pred_acc[i] != 1), 0, 1).astype(np.uint8)
+        true_acc_tmp = np.where((true_acc != 1), 0, 1).astype(np.uint8)
+
+        for j, tol in enumerate(tolerances):
+            ltiou[i, j, ...] = ltIoUMetric(tolerance=tol).forward(pred_acc[i], true_acc_tmp)
+
+    print(iou)
+    print(ltiou)
